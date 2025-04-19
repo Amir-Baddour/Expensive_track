8f09491f82f87abc48eb1bc39fad397ad159dccd
Amir-Baddour <amirbaddour@users.noreply.github.com>
2025-04-19 20:05:17 +0300

    First progect using ASP.NET Core MVC {expensive track}

---
+++ Models/ExpenseSmartDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebApplication1.Models
+{
+    public class ExpenseSmartDbContext:DbContext 
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public ExpenseSmartDbContext(DbContextOptions<ExpenseSmartDbContext> options):base (options)
+        {
+            
+        }
+    }
+}
